f2540664c54889ffa4bea4cd072c7e84fe642af6
Cléante Aubert <cleante.aubert@affluences.com>
2023-10-24 12:17:29 +0200

    Class TextEditor tests

--- src/test/java/TextEditorTest.java
+++ src/test/java/TextEditorTest.java
@@ -1,2 +1,8 @@
+import fr.einfolearning.tp2.metiers.TextEditor;
+
 public class TextEditorTest {
+    // Test de la classe "TextEditor"
+    TextEditor textEditor = new TextEditor("Hello-world");
+
+    // textEditor.setCursor()
 }
