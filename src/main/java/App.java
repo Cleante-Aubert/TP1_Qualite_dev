82fe6b47a01d0a5c5242bf5f18f7de4a37a8a1b5
bl <bouchkhaf@gmail.com>
2023-10-24 12:00:58 +0200

    Test Buffer

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,6 +11,19 @@ import fr.einfolearning.tp2.metiers.exceptions.EmacsKillRingOverflowException;
 public class App {
 
     public static void main(String[] args) {
-        // A completer
+        TextBuffer textBuffer = new TextBuffer("Hello-World");
+
+        System.out.println("Text Buffer : "+textBuffer);
+
+        // Test ins
+        textBuffer.ins("aaa",6);
+        // Résultat attendu : Hello-aaaWorld
+        System.out.println("Text BUffer après ins : "+textBuffer);
+
+        // Test sup
+        textBuffer.del(7,9);
+        // Résultat attendu : Hello-World
+        System.out.println("Text BUffer après sup : "+textBuffer);
+
     }
 }
