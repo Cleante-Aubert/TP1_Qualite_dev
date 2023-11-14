dfbfffd551974cd5ea096da271ac08afa1491417
Cléante Aubert <cleante.aubert@affluences.com>
2023-11-14 09:22:01 +0100

    Class TextEditor tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,19 +11,49 @@ import fr.einfolearning.tp2.metiers.exceptions.EmacsKillRingOverflowException;
 public class App {
 
     public static void main(String[] args) {
-        TextBuffer textBuffer = new TextBuffer("Hello-World");
+        TextBuffer text = new TextBuffer("Hello-World");
 
-        System.out.println("Text Buffer : "+textBuffer);
+        System.out.println("Text Buffer : "+text);
 
         // Test ins
-        textBuffer.ins("aaa",6);
+        text.ins("aaa",6);
         // Résultat attendu : Hello-aaaWorld
-        System.out.println("Text BUffer après ins : "+textBuffer);
+        System.out.println("Text BUffer après ins : "+text);
 
         // Test sup
-        textBuffer.del(7,9);
+        text.del(7,9);
         // Résultat attendu : Hello-World
-        System.out.println("Text BUffer après sup : "+textBuffer);
+        System.out.println("Text BUffer après sup : "+text);
+
+        TextBuffer bu = new TextBuffer("Cour qualité algo");
+        EmacsKillRing kr = new EmacsKillRing();
+        TextEditor ed = new TextEditor("Je suis un éditeur de texte");
+
+        try{
+            System.out.println(ed.getBuffer());
+
+            ed.setCursor(3);
+            ed.setMark(7);
+            ed.killRingBackup();
+            System.out.println(ed.getBuffer());
+
+            ed.setCursor(11);
+            ed.setMark(16);
+            ed.killSection();
+            System.out.println(ed.getBuffer());
+
+            ed.setCursor(8);
+            ed.yank();
+
+        ed.killSection();
+            System.out.println(ed.getBuffer());
+        }
+        catch (EmacsKillRingOverflowException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
