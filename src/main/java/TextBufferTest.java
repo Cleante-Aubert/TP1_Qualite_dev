1dbd8900285d4db58c8d9e587a900b7699de6fa3
bl <bouchkhaf@gmail.com>
2023-11-21 10:21:00 +0100

    Class Test

--- src/main/java/TextBufferTest.java
+++ src/main/java/TextBufferTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class TextBufferTest {
 
