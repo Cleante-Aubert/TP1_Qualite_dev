514de3cdffb4b4d92a5d386c3c02a829e0f2161a
Cléante Aubert <cleante.aubert@affluences.com>
2023-12-12 00:04:51 +0100

    Class TextEditor tests

--- src/test/java/EditorTest.java
+++ src/test/java/EditorTest.java
@@ -2,11 +2,8 @@ import fr.einfolearning.tp2.metiers.EmacsKillRing;
 import fr.einfolearning.tp2.metiers.TextBuffer;
 import fr.einfolearning.tp2.metiers.TextEditor;
 import fr.einfolearning.tp2.metiers.exceptions.EmacsKillRingOverflowException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import sun.security.krb5.internal.crypto.DesMacCksumType;
-
-import javax.xml.soap.Text;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -35,7 +32,6 @@ public class EditorTest {
             when(emacsKillRing.currentElt()).thenReturn("Voici un test");
             when(tb.maxP()).thenReturn(100);
             textEditor.yank();
-            textEditor.yankPop();
             verify(emacsKillRing, times(2)).currentElt();
             verify(tb).del(anyInt(), anyInt());
             verify(tb, times(2)).ins(eq("Voici un test"), anyInt());
@@ -44,20 +40,102 @@ public class EditorTest {
             System.out.println(e.toString());
         }
     }
-
+/*
     @Test
     public void test_setters_getters(){
         TextEditor te = new TextEditor("voici un nouveau test");
         verify(te.setCursor(1),"V");
         te.setMark(2);
+    }
+*/
 
+    @Test
+    void testYankPop() throws IllegalAccessException, EmacsKillRingOverflowException {
+        // Création du mock pour EmacsKillRing
+        EmacsKillRing mockEmacsKillRing = mock(EmacsKillRing.class);
 
+        // Création de l'instance de TextEditor avec le mock
+        TextEditor textEditor = new TextEditor("Sample text");
+        textEditor.setMark(0); // Marquage d'une position quelconque
+        textEditor.setCursor(5); // Position du curseur quelconque
+        textEditor.yank(); // Préparation du contexte pour yankPop
 
+        // Définition du comportement simulé pour currentElt() de l'EmacsKillRing mocké
+        when(mockEmacsKillRing.currentElt()).thenReturn("Mocked Text");
+
+        // Injection du mock dans la classe à tester
+        textEditor.emacsKillring = mockEmacsKillRing;
+
+        // Appel de la méthode à tester
+        textEditor.yankPop();
+
+        // Vérification que rotateFwd() a bien été appelé sur le mock
+        verify(mockEmacsKillRing, times(1)).rotateFwd();
+
+        // assertEquals("Expected Result", textEditor.getBuffer());
     }
 
+    @Test
+    void testYankPopAfterYank() throws IllegalAccessException, EmacsKillRingOverflowException {
+        // Création du mock pour EmacsKillRing
+        EmacsKillRing mockEmacsKillRing = mock(EmacsKillRing.class);
 
+        // Création de l'instance de TextEditor avec le mock
+        TextEditor textEditor = new TextEditor("Sample text");
+        textEditor.setMark(0); // Marquage d'une position quelconque
+        textEditor.setCursor(5); // Position du curseur quelconque
 
+        // Définition du comportement simulé pour currentElt() de l'EmacsKillRing mocké
+        when(mockEmacsKillRing.currentElt()).thenReturn("Mocked Text");
 
+        // Injection du mock dans la classe à tester
+        textEditor.emacsKillring = mockEmacsKillRing;
+
+        // Appel de la méthode yank
+        textEditor.yank();
+
+        // Appel de la méthode yankPop
+        textEditor.yankPop();
+
+        // Vérification que rotateFwd() a bien été appelé sur le mock
+        verify(mockEmacsKillRing, times(1)).rotateFwd();
+
+        // Vérification que le texte du buffer est conforme aux attentes après l'exécution de yankPop
+        // assertEquals("Expected Result", textEditor.getBuffer());
+    }
+    }
+
+    /*
+    Idée de test question KillRegion
+
+    @Test
+    void testKillRegion() throws IllegalAccessException, EmacsKillRingOverflowException {
+        // Création des mocks pour TextBuffer et EmacsKillRing
+        TextBuffer mockTextBuffer = mock(TextBuffer.class);
+        EmacsKillRing mockEmacsKillRing = mock(EmacsKillRing.class);
+
+        // Création de l'instance de TextEditor avec les mocks
+        TextEditor textEditor = new TextEditor(mockTextBuffer, mockEmacsKillRing);
+        textEditor.setMark(0); // Marquage d'une position quelconque
+        textEditor.setCursor(5); // Position du curseur quelconque
+
+        // Définition du comportement simulé pour getText et add de TextBuffer et EmacsKillRing
+        when(mockTextBuffer.substr(0, 5)).thenReturn("Mocked Text");
+        doNothing().when(mockEmacsKillRing).add(anyString());
+
+        // Appel de la méthode à tester
+        textEditor.killRegion();
+
+        // Création d'un ordre d'appel pour vérifier la séquence des appels
+        InOrder inOrder = inOrder(mockTextBuffer, mockEmacsKillRing);
+
+        // Vérification que getText est appelé en premier
+        inOrder.verify(mockTextBuffer).substr(0, 5);
+
+        // Vérification que add est appelé ensuite avec le message récupéré du TextBuffer
+        inOrder.verify(mockEmacsKillRing).add("Mocked Text");
+    }
+    */
 
 
 }
